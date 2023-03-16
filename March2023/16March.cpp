2598250ee71932235fd598aa26f0fe3b984c1bb5
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-03-17 00:26:45 +0530

    Update 16March.cpp

--- March2023/16March.cpp
+++ March2023/16March.cpp
@@ -32,3 +32,64 @@ public:
         return ans;
     }
 };
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    
+    void solve(int n,int from,int to,int aux,long long int &count)
+    {
+        if(n==1)
+        {
+            cout<<"move disk "<<n<<" from rod "<< from<<" to rod "<<to<<endl;
+            count=count+1;
+            return;
+        }
+        
+        solve(n-1,from,aux,to,count);
+        cout<<"move disk "<<n<<" from rod "<<from<<" to rod "<<to<<endl;
+        count=count+1;
+        solve(n-1,aux,to,from,count);
+        
+    }
+    // You need to complete this function
+
+    // avoid space at the starting of the string in "move disk....."
+    long long toh(int n, int from, int to, int aux) {
+        // Your code here
+        long long int count=0;
+        solve(n,from,to,aux,count);
+        return count;
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;//testcases
+    while (T--) {
+        
+        int N;
+        cin >> N;//taking input N
+        
+        //calling toh() function
+        Solution ob;
+        
+        cout << ob.toh(N, 1, 3, 2) << endl;
+    }
+    return 0;
+}
+
+
+//Position this line where user code will be pasted.
+// } Driver Code Ends
