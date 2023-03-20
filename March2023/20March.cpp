e7dbc02a1e32b4ae73f07e39118c24270ccef538
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-03-20 23:27:34 +0530

    Update 20March.cpp

--- March2023/20March.cpp
+++ March2023/20March.cpp
@@ -120,3 +120,58 @@ void printRepeating(int arr[], int n)
         return maxi;
     }
 };
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    long maximumSumSubarray(int k, vector<int> &arr , int n){
+     
+        // code 
+        int i=0,j=0;
+        long int sum=0;
+        long int maxi=INT_MIN;
+        
+        while(j<n)
+        {
+            sum+=arr[j];
+            if(j-i+1<k)
+            {
+                j++;
+            }
+            else if(j-i+1==k)
+            {
+                maxi=max(maxi,sum);
+                sum-=arr[i];
+                i++;j++;
+            }
+        }
+        return maxi;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,K;
+        cin >> N >> K;;
+        vector<int>Arr;
+        for(int i=0;i<N;++i){
+            int x;
+            cin>>x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
