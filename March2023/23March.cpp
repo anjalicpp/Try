11742bcf2eba8f682406672a0e16fe0092114f38
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-03-23 23:31:19 +0530

    Update 23March.cpp

--- March2023/23March.cpp
+++ March2023/23March.cpp
@@ -48,3 +48,52 @@ public:
         return ans;
     }
 };
+
+problem 567
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+    map<char,int>mp;
+     for(auto it:s1)
+     {
+         mp[it]++;
+     } 
+
+     int i=0,j=0,ans=0;
+     int k=s1.size();
+     int count=mp.size();
+
+     while(j<s2.size())
+     {
+         if(mp.find(s2[j])!=mp.end())
+         {
+             mp[s2[j]]--;
+             if(mp[s2[j]]==0)
+             {
+                 count--;
+             }
+         }
+
+         if(j-i+1<k)
+         {
+             j++;
+         }
+         else if(j-i+1==k)
+         {
+             if(count==0)
+             {
+                 ans++;
+             }
+             if(mp.find(s2[i])!=mp.end())
+             {
+                 mp[s2[i]]++;
+                 if(mp[s2[i]]==1)
+                 count++;
+             }
+             i++;j++;
+         }
+     } 
+    if(ans)return true;
+    else return false; 
+    }
+};
