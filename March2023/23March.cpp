8c1721b1e57d610541f0e280c2f082155d504213
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-03-23 23:42:15 +0530

    Update 23March.cpp

--- March2023/23March.cpp
+++ March2023/23March.cpp
@@ -172,3 +172,31 @@ int main() {
     return 0;
 }
 // } Driver Code Ends
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       map<char,int>mp;
+       for(auto it:s)
+       {
+           mp[it]++;
+       }
+
+       int i=0;
+       int count=mp.size();
+       while(i<t.size())
+       {
+           if(mp.find(t[i])==mp.end())
+           {
+               return false;
+           }
+           else {
+               mp[t[i]]--;
+               if(mp[t[i]]==0)
+               count--;
+           }
+           i++;
+       }
+       if(count==0)return true;
+       else return false;
+    }
+};
