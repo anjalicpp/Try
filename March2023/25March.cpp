59bc5978e703d09eb8088620be0de3109bf62ca3
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-03-25 14:43:03 +0530

    Create 25March.cpp

---
+++ March2023/25March.cpp
@@ -0,0 +1,50 @@
+448. Find All Numbers Disappeared in an Array
+Easy
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        map<int,int>mp;
+        vector<int> ans;
+        for(auto it:nums)
+        {
+            mp[it]++;
+        }
+
+        for(int i=1;i<=nums.size();i++)
+        {
+            if(mp.find(i)==mp.end())
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+     sort(nums.begin(),nums.end());
+     vector<int>ans;
+    int k=2;
+      int i=0,j=0;
+      while(j<nums.size())
+      {
+          if(j-i+1<k)
+          {
+              j++;
+          }
+          else if(j-i+1==k)
+          {
+              if(nums[i]==nums[j])
+              ans.push_back(nums[i]);
+              i++;j++;
+          }
+      }
+      return ans;  
+    }
+};
