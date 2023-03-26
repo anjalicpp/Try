8bd555501c188714c1cd2250eaa5957c30879a1c
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-03-26 22:45:17 +0530

    Update 26March.cpp

--- March2023/26March.cpp
+++ March2023/26March.cpp
@@ -112,3 +112,52 @@ public:
       return ans;  
     }
 };
+
+107. Binary Tree Level Order Traversal II
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+         vector<vector<int>> ans;
+         stack<vector<int>>s;
+        queue<TreeNode*>q;
+       // vector<int>level;
+        
+        if(root==NULL)
+            return ans;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size=q.size();
+            vector<int>level;
+            for(int i=0;i<size;i++)
+            {
+               TreeNode*temp=q.front();
+                q.pop();
+                if(temp->left!=NULL)q.push(temp->left);
+                if(temp->right!=NULL)q.push(temp->right);
+                //q.pop();
+                level.push_back(temp->val);
+                
+            }
+           s.push(level); 
+        }
+        while(!s.empty())
+        {
+            ans.push_back(s.top());
+            s.pop();
+        }
+      return ans;  
+    }
+};
