ea913715d122cfbe4676733fc5566fe303751a57
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-03-27 20:54:36 +0530

    Create 27 March.cpp

---
+++ March2023/27 March.cpp
@@ -0,0 +1,36 @@
+3. Longest Substring Without Repeating Characters
+Medium
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int i=0,j=0;
+        map<int,int>mp;
+        int maxi=0;
+        while(j<s.size())
+        {
+            mp[s[j]]++;
+
+            if(mp.size()==j-i+1)
+            {
+                maxi=max(maxi,j-i+1);
+                j++;
+            }
+
+            else if(mp.size()<j-i+1)
+            {
+                while(mp.size()<j-i+1)
+                {
+                    mp[s[i]]--;
+                    if(mp[s[i]]==0)
+                    {
+                        mp.erase(s[i]);
+                    }
+                    i++;
+                }
+                j++;
+            }
+            
+        }
+        return maxi;
+    }
+};
