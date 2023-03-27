7a7c7c00c23f8b1879043ca6e4d5d8a6d41e809c
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-03-27 20:58:32 +0530

    Update 27 March.cpp

--- March2023/27 March.cpp
+++ March2023/27 March.cpp
@@ -36,3 +36,46 @@ public:
 };
 
 problem 992
+    
+    
+876. Substrings of Size Three with Distinct Characters
+Easy
+1.1K
+28
+
+    
+    class Solution {
+public:
+    int countGoodSubstrings(string s) {
+       int i=0,j=0;
+       int count=0;
+       map<char,int> mp;
+       while(j<s.size())
+       {
+           mp[s[j]]++;
+           if(j-i+1<3)
+           {
+               j++;
+           }
+           else if(j-i+1==3)
+           {
+            if(mp.size()==3)
+            {
+                count++;
+            }
+            
+            mp[s[i]]--;
+            if(mp[s[i]]==0)
+            {
+                mp.erase(s[i]);
+            }
+            i++;
+            j++;
+
+           }
+
+          
+       } 
+       return count;
+    }
+};
