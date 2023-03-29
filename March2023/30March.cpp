a89f57f49009f4a03e42e13fc30787f211cd3c19
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-03-30 00:19:19 +0530

    Update 30March.cpp

--- March2023/30March.cpp
+++ March2023/30March.cpp
@@ -24,3 +24,29 @@ public:
         return ans;
     }
 };
+
+2574. Left and Right Sum Differences
+Easy
+
+class Solution {
+public:
+    vector<int> leftRigthDifference(vector<int>& nums) {
+        
+       vector <int> leftSum;
+        vector <int> rightSum;
+        vector <int> ans;
+        int left_sum=0;
+        int right_sum=0;
+        for(int i=0;i<nums.size();i++){
+                leftSum.push_back(left_sum);
+                rightSum.push_back(right_sum);
+                left_sum+=nums[i];
+                right_sum+=nums[nums.size()-1-i];
+        }
+        for(int i=0;i<nums.size();i++){
+            cout<<leftSum[i]<<" "<<rightSum[i]<<endl;
+            ans.push_back(abs(leftSum[i]-rightSum[nums.size()-1-i]));
+        }
+        return ans;
+    }
+};
