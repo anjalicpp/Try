1afba9e29bbb9f157ee5b21c30c0005d2413a649
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-03-30 00:21:31 +0530

    Update 30March.cpp

--- March2023/30March.cpp
+++ March2023/30March.cpp
@@ -92,3 +92,23 @@ public:
      return ans;
     }
 };
+215. Kth Largest Element in an Array
+Medium
+13.5K
+669
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>>mh;
+        for(int i=0;i<nums.size();i++)
+        {
+            mh.push(nums[i]);
+            if(mh.size()>k)
+            {
+                mh.pop();
+            }
+        }
+        return mh.top();
+    }
+
+};
