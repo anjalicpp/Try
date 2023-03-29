33d714ffb530c3b943346805bb20a252a38f4973
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-03-29 15:17:22 +0530

    Update 29March.cpp

--- March2023/29March.cpp
+++ March2023/29March.cpp
@@ -25,3 +25,41 @@ public:
        return ans/k;
     }
 };
+
+703. Kth Largest Element in a Stream
+
+class KthLargest {
+public:
+        priority_queue<int,vector<int>,greater<int>> m;
+        int K;
+    KthLargest(int k, vector<int>& nums) {
+        K=k;
+         for(int i=0;i<nums.size();i++)
+        {
+            m.push(nums[i]);
+            if(m.size()>k)
+            {
+            m.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        
+       
+            m.push(val);
+            if(m.size()>K)
+            {
+            m.pop();
+            }
+       
+        return m.top();
+
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
