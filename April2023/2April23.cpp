56ac3b1ac6163e20aed23870c76f1d1197beb026
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-04-03 00:20:25 +0530

    Update 2April23.cpp

--- April2023/2April23.cpp
+++ April2023/2April23.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(),potions.end());
+        vector<int>ans;
+        int count=0;
+        long long int check;
 
+        for(int i=0;i<spells.size();i++)
+        {
+            count=0;
+            for(int j=0;j<potions.size();j++)
+            {
+                check=spells[i]*potions[j];
+                if(check>=success)
+                {
+                   count++;
+                }
+               
+
+            }
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};
