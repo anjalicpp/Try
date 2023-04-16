4f84a435b5113164191195c3722387689f4fb885
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-04-16 22:58:24 +0530

    Update 16April23.cpp

--- April2023/16April23.cpp
+++ April2023/16April23.cpp
@@ -1,3 +1,5 @@
+236. Lowest Common Ancestor of a Binary Tree
+Medium
 
 /**
  * Definition for a binary tree node.
@@ -24,5 +26,34 @@ public:
         else return root;
 
 
+    }
+};
+
+235. Lowest Common Ancestor of a Binary Search Tree
+Medium
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+       if(root==NULL||root==p||root==q)
+        {
+            return root;
+        }
+
+        TreeNode*left=lowestCommonAncestor(root->left,p,q);
+        TreeNode*right=lowestCommonAncestor(root->right,p,q);
+
+        if(left==NULL)return right;
+        if(right==NULL)return left;
+        else return root; 
     }
 };
