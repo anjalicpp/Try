871683095a08b570dbe678f77f3af75cf181586c
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-04-17 23:03:30 +0530

    Update 17April23.cpp

--- April2023/17April23.cpp
+++ April2023/17April23.cpp
@@ -19,3 +19,20 @@ public:
         
     }
 };
+
+
+
+
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int n=arr.size();
+        int ans=0;
+      for(int i=0;i<n;i++)
+		{
+            int contribution = ceil((i+1)*(n-i)/2.0);
+            ans+=(contribution*arr[i]);
+        }
+        return ans;  
+    }
+};
