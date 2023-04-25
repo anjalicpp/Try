dab352887ae21349889ab344cbcaede61581cf0b
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-04-25 23:04:52 +0530

    Update 25April23.cpp

--- April2023/25April23.cpp
+++ April2023/25April23.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+       
+     
+     int left=0,right;
+        for(right=1;right<nums.size();right++)
+        {
+            if(nums[left]!=nums[right])
+                left++;
+            nums[left]=nums[right];
+        }
+        return left+1;
+       /* map<int,int>mp;
+        for (auto it:nums)
+        {
+            mp[it]++;
+        }
+        int i=0;
+        for(auto it:mp)
+        {
+            nums[i++]=it.first;
+        }
+        return nums.size();*/
+    }
+};
