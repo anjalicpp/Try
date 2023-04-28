40fd13a194235b32db77b846092654e2961e8b54
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-04-28 22:45:14 +0530

    Create 28April23.cpp

---
+++ April2023/28April23.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        if(n==1)return 1;
+        int sum=(n*(n+1))/2;
+        int sum2=0;
+        for(int i=n;i>=1;i--)
+        {
+           // sum=sum-i;
+            sum2+=i;
+            if(sum==sum2)
+            {
+                return i;
+            }
+             sum=sum-i;
+        }
+        return -1;
+    }
+};
