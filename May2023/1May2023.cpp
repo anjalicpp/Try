de944f61a15c4a19d40b15565535d30fe3bbfcad
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-05-01 23:23:11 +0530

    Update 1May2023.cpp

--- May2023/1May2023.cpp
+++ May2023/1May2023.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        sort(salary.begin(),salary.end());
+        double ans=0;
+        int size=salary.size()-2;
+        for(int i=1;i<=size;i++)
+        {
+            ans+=salary[i];
+        }
+        return ans/size; 
+    }
+};
