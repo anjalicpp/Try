3b1c60c5be5a80955badaabf259769782aace716
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-05-11 23:17:49 +0530

    Update 11May23.cpp

--- May2023/11May23.cpp
+++ May2023/11May23.cpp
@@ -39,3 +39,30 @@ public:
      return ans;
     }
 };
+
+229. Majority Element II
+Medium
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int limit=floor(nums.size()/3);
+        vector<int> ans;
+       map<int,int>mp;
+      for(auto it:nums)
+      {
+          mp[it]++;
+      } 
+      priority_queue<pair<int,int>>mh;
+      for(auto i:mp)
+      {
+          mh.push({i.second,i.first});
+      }
+        while(mh.size()>0)
+        {
+            if(mh.top().first>limit)
+            ans.push_back(mh.top().second);
+            mh.pop();
+        } 
+        return ans;
+    }
+};
