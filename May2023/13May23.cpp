d00ac7934e555b462f4e7aabc2fe7be3f5d7b81b
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-05-13 13:37:04 +0530

    Create 13May23.cpp

---
+++ May2023/13May23.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+         if(x<0)return false;
+        
+        else{
+       long int rem,rev=0,num=x;
+        while(num)
+        {
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+            
+        }
+       if(rev==x) return true;
+        return false;
+    }}
+    
+};
