1b7df8f0b82e6c64ed77c18ce93dc3c529b75cc9
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-05-21 23:21:51 +0530

    Update 21May23.cpp

--- May2023/21May23.cpp
+++ May2023/21May23.cpp
@@ -1,1 +1,29 @@
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool symetery(TreeNode* p,TreeNode* q)
+    {
+        if(p==NULL&&q==NULL)return true;
+        
+        if((p==NULL&&q!=NULL)||(q==NULL&&p!=NULL))return false;
+        
+        if(p->val!=q->val)return false;
+        return (symetery(p->right,q->left)&&symetery(p->left,q->right));
+    }
+    bool isSymmetric(TreeNode* root) {
+       
+       return symetery(root->left,root->right);
+    }
+};
