b891623a43558cba5c1a3c4a967c83e669f4cfff
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-05-22 23:49:31 +0530

    Update 22May23.cpp

--- May2023/22May23.cpp
+++ May2023/22May23.cpp
@@ -1,1 +1,30 @@
 
+class Solution {
+public:
+//typedef pair<int,pair<int,int>>ppi;
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> mh;
+        vector<int> ans;
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+       
+       for(auto it:mp)
+       {
+           mh.push({it.second,it.first});
+           if(mh.size()>k)
+           {
+               mh.pop();
+           }
+       }
+       while(mh.size()>0)
+       {
+           ans.push_back(mh.top().second);
+           mh.pop();
+       }
+        return ans;
+    }
+};
