f9e4999e34217d674fd513ee2e06836e9d6c651c
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-05-23 22:32:34 +0530

    Create 23May23.cpp

---
+++ May2023/23May23.cpp
@@ -0,0 +1,35 @@
+class KthLargest {
+public:
+        priority_queue<int,vector<int>,greater<int>> m;
+        int K;
+    KthLargest(int k, vector<int>& nums) {
+        K=k;
+         for(int i=0;i<nums.size();i++)
+        {
+            m.push(nums[i]);
+            if(m.size()>k)
+            {
+            m.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        
+       
+            m.push(val);
+            if(m.size()>K)
+            {
+            m.pop();
+            }
+       
+        return m.top();
+
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
