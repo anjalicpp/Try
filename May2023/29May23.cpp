14d4b63c85a1f3b25eff01c1178eec59b1c8f1eb
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-05-29 23:15:14 +0530

    Update 29May23.cpp

--- May2023/29May23.cpp
+++ May2023/29May23.cpp
@@ -1,1 +1,38 @@
 
+class Solution {
+public:
+    void solve(int index,int target,vector<int>&nums,vector<int>output,vector<vector<int>>&ans)
+    {
+        if(index==nums.size())
+        {
+            if(target==0)
+            {
+                ans.push_back(output);
+            }
+            return;
+        }
+        
+        //pick wali condition
+        
+        if(nums[index]<=target)
+        {
+            output.push_back(nums[index]);
+            solve(index,target-nums[index],nums,output,ans);
+            output.pop_back();
+            
+        }
+        
+        //not pick wali condition
+        
+        solve(index+1,target,nums,output,ans);
+        
+    } 
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+     
+           vector<vector<int>> ans;
+           vector<int> output;
+        solve(0,target,candidates,output,ans);
+        return ans;
+    }
+};
