0166820d4463c768123085c2129b553eb7ec2349
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-04 23:32:47 +0530

    Update 4June23.cpp

--- June2023/4June23.cpp
+++ June2023/4June23.cpp
@@ -1,1 +1,32 @@
 
+class Solution {
+public:
+    void solve(vector<int>& nums,vector<vector<int>>&ans,int index)
+    {
+        if(index>=nums.size())
+        { 
+            ans.push_back(nums);
+            return;
+        }
+        
+        //swaping and solving
+        
+        for(int j=index;j<nums.size();j++)
+        {
+            swap(nums[index],nums[j]);
+            solve(nums,ans,index+1);
+            //backtracing
+            swap(nums[index],nums[j]);
+            
+        }
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums)
+    {
+         vector<vector<int>>ans;
+        int index=0;
+        solve(nums,ans,index);
+        return ans;
+    }
+};
