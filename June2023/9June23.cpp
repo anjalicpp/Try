de49ca9ae4862205863ac2a8a98a182011898c33
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-09 20:42:18 +0530

    Update 9June23.cpp

--- June2023/9June23.cpp
+++ June2023/9June23.cpp
@@ -1,1 +1,25 @@
+744. Find Smallest Letter Greater Than Target
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+ 
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+       for(auto i:letters)
+       {
+           if(i>target)
+           return i;
+       }
+       return letters[0];
+    }
+Easy
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+ };
+
 
