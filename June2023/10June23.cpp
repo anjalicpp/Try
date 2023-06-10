bb79e8f859889117ce2ebe1afc89985a3fc229ac
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-10 23:01:34 +0530

    Update 10June23.cpp

--- June2023/10June23.cpp
+++ June2023/10June23.cpp
@@ -1,1 +1,55 @@
+class MinStack {
+public: stack<int> s;
+        stack<int> ss;
+ 
 
+    MinStack() {
+      
+ 
+    }
+    
+    void push(int val) {
+       if(s.size()==0||ss.top()>=val)
+       {
+           ss.push(val);
+       } 
+       s.push(val);
+    }
+    
+    void pop() {
+       
+      int temp=s.top();
+      s.pop();
+
+      if(ss.top()==temp)
+      {
+          ss.pop();
+      }  
+    }
+    
+    int top()
+    {
+        if(s.size()==0)
+        {
+            return -1;
+        }
+       return s.top(); 
+    }
+    
+    int getMin() {
+        if(s.size()==0)
+        {
+            return -1;
+        }
+       return ss.top(); 
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
