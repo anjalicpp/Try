64c32fbb4ede1d99fce941e3d49d94dc28e42a2d
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-15 23:31:49 +0530

    Update 15June23.cpp

--- June2023/15June23.cpp
+++ June2023/15June23.cpp
@@ -1,1 +1,44 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+       int i=0,j=0;
+        int ans=INT_MAX;
+        int sum=0;
 
+       while(j<nums.size())
+       {
+           sum+=nums[j];
+           if(sum<target)
+           {
+               j++;
+           }
+          else if(sum==target)
+            {
+                
+                ans=min(ans,j-i+1);
+                j++;
+            }
+            else if(sum>target)
+            {
+               // ans=min(ans,j-i+1);
+                while(sum>target)
+                {
+                     ans=min(ans,j-i+1);
+                    sum=sum-nums[i];
+                    i++;
+                     if(sum==target)
+                {
+                    ans=min(ans,j-i+1);
+                }
+                    
+                    
+                }
+                
+                j++;
+            }
+
+       } 
+       if(ans==INT_MAX) return 0;
+       return ans;
+    }
+};
