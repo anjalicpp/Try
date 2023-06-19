cd430f387c7cf9222244107e0eb203bbb652a368
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-19 20:06:58 +0530

    Update 19June23.cpp

--- June2023/19June23.cpp
+++ June2023/19June23.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+       vector<int>ans;
+       int x;
+       ans.push_back(0);
+       int val=0;
+       for(auto i:gain)
+       {
+           val+=i;
+           ans.push_back(val);
+       } 
 
+        x=*max_element(ans.begin(),ans.end());
+        return x;
+    }
+};
