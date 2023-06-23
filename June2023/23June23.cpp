9a1d0b0ab100bac3f655ee462917475de39c7e35
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-23 23:52:14 +0530

    Update 23June23.cpp

--- June2023/23June23.cpp
+++ June2023/23June23.cpp
@@ -1,1 +1,22 @@
+7. Reverse Integer
+Medium
 
+class Solution {
+public:
+    int reverse(int x) {
+        
+        
+        long long ans=0,rem=0,val=1;
+        if(x<0) val=-1;
+        x=abs(x);
+        while(x)
+        {
+            rem=x%10;
+            ans=ans*10+rem;
+            x=x/10;
+        }
+        val=val*ans;
+        if(val>=2147483647||val<=-2147483648)return 0;
+        return int(val);
+    }
+};
