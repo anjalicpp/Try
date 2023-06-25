2a56f8082109363f74795640d05c5f719978074b
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-25 21:19:02 +0530

    Update 25June23.cpp

--- June2023/25June23.cpp
+++ June2023/25June23.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int i=0,j=0;
+        int s=needle.size();
+       while(j<haystack.size())
+       {
+          // return 5;
+           if(needle[i]==haystack[j])
+           {
+                if(i==s-1)return j-s+1;
 
+               i++;j++;
+           }
+           else {
+               j=j-i+1;
+               i=0;
+           }
+           //if(i==s-1)return j-s;
+
+        }
+        return -1;
+    }
+};
