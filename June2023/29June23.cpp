baea5c2fea7c9df721a6d4972873922f0c1e934d
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-29 23:07:58 +0530

    Update 29June23.cpp

--- June2023/29June23.cpp
+++ June2023/29June23.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str;
+     for(auto i:s)
+     {
+         if(isalnum(i))
+         {
+             str+=tolower(i);
+         }
+    } 
 
+    int i=0;
+    int j=str.length()-1;
+   // if(str.length()==1)return false;
+    while(i<j)
+    {
+        if(str[i]!=str[j])
+        return false;
+        i++;j--;
+    }
+    return true;
+
+    }
+};
