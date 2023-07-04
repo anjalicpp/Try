566ccf528c9ff0821099c4c618c7fd5ea9e30199
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-07-04 23:09:12 +0530

    Update 4July23.cpp

--- July2023/4July23.cpp
+++ July2023/4July23.cpp
@@ -1,1 +1,15 @@
-
+137. Single Number II
+Medium
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int result;
+        for(int i=0;i<nums.size()-1;i=i+3)
+        {
+            result=nums[i]^nums[i+1];
+            if(result!=0)return nums[i];
+        }
+        return nums[nums.size()-1];
+    }
+};
