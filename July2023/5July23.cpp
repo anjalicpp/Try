08dfd7d6e3f7f2792c8ec2e209d348d1af5ae478
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-07-06 00:00:51 +0530

    Update 5July23.cpp

--- July2023/5July23.cpp
+++ July2023/5July23.cpp
@@ -1,1 +1,39 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+       int i=0;
+       int j=0;
+       int count_zero=0;
+       int maxi=0;
 
+
+       while(j<nums.size())
+       {
+           if(nums[j]==0)
+           count_zero++;
+
+          /* if(count_zero<2)
+           {
+               j++;
+           }*/
+           if(count_zero>=2)
+           {
+               maxi=max(maxi,j-i-1);
+               while(nums[i]!=0)
+               {
+                   i++;
+               }
+                    i++;
+                   count_zero=1;j++;
+            }
+           else {
+                maxi=max(maxi,j-i);
+                j++;
+               }
+
+          // maxi=max(maxi,j-i+1);
+       } 
+       if(count_zero==0)return maxi;
+       return maxi;
+    }
+};
