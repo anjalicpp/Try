20c88c4cac3de40b1aed584cd0f8f222456a3901
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-07-08 22:08:26 +0530

    Update 8July23.cpp

--- July2023/8July23.cpp
+++ July2023/8July23.cpp
@@ -1,3 +1,25 @@
+/*
+/*APPROACH BLOCK
+  while(j<answerKey.length())
+    {
+        if(answerKey[j]=='T')t++;
+        else f++;
+
+        while(t>k&&f>k)
+        {
+            if(answerKey[i]=='T')
+            t--;
+            else f--;
+            i++;
+        }
+        maxi=max(maxi,j-i+1);
+        j++;
+    }
+    return maxi;
+*/
+
+*/
+
 485. Max Consecutive Ones
 Easy
 
@@ -25,21 +47,34 @@ public:
         return maxi;
     }
 };
-/*APPROACH BLOCK
-  while(j<answerKey.length())
-    {
-        if(answerKey[j]=='T')t++;
-        else f++;
+1004. Max Consecutive Ones III
+Medium
+6.9K
+73
+Companies
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+    class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i=0;
+        int j=0;
+        int maxi=0;
+        int ones=0;
+        int zeros=0;
 
-        while(t>k&&f>k)
+        while(j<nums.size())
         {
-            if(answerKey[i]=='T')
-            t--;
-            else f--;
-            i++;
+            if(nums[j]==0)zeros++;
+            else ones++;
+            while(zeros>k)
+            {
+                if(nums[i]==0)zeros--;
+                else ones--;
+                i++;
+            }
+            maxi=max(maxi,j-i+1);
+            j++;
         }
-        maxi=max(maxi,j-i+1);
-        j++;
+        return maxi;
     }
-    return maxi;
-*/
+};
