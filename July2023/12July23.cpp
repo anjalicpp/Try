6542d281b7389cf3809969879280b57e1b9e3f9e
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-07-12 21:46:15 +0530

    Create 12July23.cpp

---
+++ July2023/12July23.cpp
@@ -0,0 +1,8 @@
+219. Contains Duplicate II
+Easy
+5.1K
+2.7K
+Companies
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
