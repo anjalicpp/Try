4d6ca9b6f91eba0e19486755e1559e287d684cf5
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-07-25 00:08:24 +0530

    Update 25July23.cpp

--- July2023/25July23.cpp
+++ July2023/25July23.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0)return 1;
+        if(n==1)return x;
+        if(n>0)//positive powers
+        {
+            if(n%2==0)//even power
+            return myPow(x*x,n/2);
+            else return x*myPow(x*x,n/2);
+        }
+        else{//negative case
+            n=abs(n);//to convert into positive integer
+            if(n%2==0)return 1/(myPow(x*x,n/2));
+            else return 1/(x*myPow(x*x,n/2));
+        }
+    }
+};
