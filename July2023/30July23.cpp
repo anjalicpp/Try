a652938d643ddb7d6fffc13574d07d722a62f576
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-07-30 23:37:50 +0530

    Update 30July23.cpp

--- July2023/30July23.cpp
+++ July2023/30July23.cpp
@@ -1,1 +1,70 @@
+1020. Number of Enclaves
 
+You are given an m x n binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
+A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
+Return the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves.
+
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        queue<pair<int,int>>q;
+        int n=grid.size();
+        int m=grid[0].size();
+
+        int vis[n][m];
+
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0||j==0||i==n-1||j==m-1)//for first,last row&&first ,last col
+                {
+                    if(grid[i][j]==1)
+                    {
+                        q.push({i,j});
+                        vis[i][j]=1;
+                    }
+                }
+                else vis[i][j]=0;
+            }
+        }
+
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,+1,0,-1};
+
+        while(!q.empty())//bfs
+        {
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+
+            for(int i=0;i<4;i++)
+            {
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
+                 vis[nrow][ncol]==0 && grid[nrow][ncol]==1)
+                 {
+                     vis[nrow][ncol]=1;
+                     q.push({nrow,ncol});
+                 }
+            }
+        }
+        int count=0;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1 && vis[i][j]==0)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+
+    }
+};
