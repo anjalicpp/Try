5ad0385a94daa796aa3c8a9194e358ae24041f07
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-08-02 18:39:08 +0530

    Update 2Aug23.cpp

--- August2023/2Aug23.cpp
+++ August2023/2Aug23.cpp
@@ -1,1 +1,32 @@
-
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+class Solution {
+public:
+    void solve(vector<int>& nums,vector<vector<int>>&ans,int index)
+    {
+        if(index>=nums.size())
+        { 
+            ans.push_back(nums);
+            return;
+        }
+        
+        //swaping and solving
+        
+        for(int j=index;j<nums.size();j++)
+        {
+            swap(nums[index],nums[j]);
+            solve(nums,ans,index+1);
+            //backtracing
+            swap(nums[index],nums[j]);
+            
+        }
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums)
+    {
+         vector<vector<int>>ans;
+        int index=0;
+        solve(nums,ans,index);
+        return ans;
+    }
+};
