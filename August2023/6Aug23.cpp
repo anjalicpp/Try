d0cc3a1c4c13d1e49b37b315a9d4a4d7a0e5357a
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-08-06 23:41:10 +0530

    Update 6Aug23.cpp

--- August2023/6Aug23.cpp
+++ August2023/6Aug23.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+      int ans=0;
+      while(k)
+      {
+          if(numOnes)
+          {
+              ans=ans+1;
+              numOnes--;
+          }
+          else if(numZeros)
+          {
+              ans=ans;
+              numZeros--;
+          }
+          else {
+              ans=ans-1;
+              numNegOnes--;
+          }
+          k--;
+      } 
+      return ans; 
+    }
+};
