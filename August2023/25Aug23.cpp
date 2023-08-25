6ac64080d5e009d4d9fd54a11ef99c0b8d2ab2cd
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-08-25 17:31:10 +0530

    Update 25Aug23.cpp

--- August2023/25Aug23.cpp
+++ August2023/25Aug23.cpp
@@ -46,3 +46,32 @@ public:
        return ans; 
     }
 };
+
+-----------------------
+
+ 2236. Root Equals Sum of Children
+
+You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+       int childrensum=0;
+       childrensum=root->left->val+root->right->val;
+       int rootvalue=0;
+       rootvalue=root->val;
+       return rootvalue==childrensum; 
+    }
+};
