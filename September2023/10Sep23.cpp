b75b97011c6472789c1b5c2e597b9f03d85b2002
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-09-10 23:26:15 +0530

    Update 10Sep23.cpp

--- September2023/10Sep23.cpp
+++ September2023/10Sep23.cpp
@@ -1,1 +1,41 @@
-
+class Solution {
+public:
+    bool palindrome(string s,int st,int e)//paloindrome checking
+    { while(st<=e)
+         {
+        if(s[st++]!=s[e--])
+            return false;
+        }
+     return true;
+    }
+    
+    void func(int index,string s, vector<vector<string>>&res,vector<string>&path)
+    {
+        if(index==s.size())  //base case
+        {
+            res.push_back(path);
+            return;
+        }
+        
+        for(int i=index;i<s.size();i++)
+        {
+            if(palindrome(s,index,i))
+            {
+                path.push_back(s.substr(index,i-index+1));//substring to be inserted in path
+                func(i+1,s,res,path);//recursive call
+                path.pop_back();//backtrack
+            }
+            
+        }
+    }
+    
+    vector<vector<string>> partition(string s)
+    {
+        vector<vector<string>>res;
+        vector<string>path;
+        func(0,s,res,path);
+        return res;
+    }
+        
+    
+};
