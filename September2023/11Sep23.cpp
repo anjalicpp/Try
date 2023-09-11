702e8239715eb615223397b4e485791347ffab88
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-09-12 00:13:25 +0530

    Update 11Sep23.cpp

--- September2023/11Sep23.cpp
+++ September2023/11Sep23.cpp
@@ -1,1 +1,50 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+     /*map<int,int>mp;
+     vector<vector<int>>v;
+     for(auto it:groupSizes)
+     {
+         mp[it]++;
+     }   
 
+     for(auto a:mp)
+     {
+         int nov=a.second/a.first;
+         int k=0;
+         while(nov>0)
+         {
+           vector<int>temp;
+           while(temp.size()<a.first)
+           {
+               if(groupSizes[k++]==a.first)
+               {
+                   temp.push_back(k-1);
+               }
+           } 
+           v.push_back(temp);
+           nov--; 
+         }
+
+
+     }
+
+     return v;*/
+
+
+      //BETTER CLEAN APPROACH
+
+     map<int,vector<int>>mp;
+     vector<vector<int>>ans;
+     for(auto it=0;it<groupSizes.size();it++)
+     {
+         mp[groupSizes[it]].push_back(it);
+         if(mp[groupSizes[it]].size()==groupSizes[it])
+         {
+             ans.push_back(mp[groupSizes[it]]);
+             mp[groupSizes[it]]={};
+         }
+     }
+return ans;
+    }
+};
