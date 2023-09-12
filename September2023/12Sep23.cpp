92f310ab52ab49fa26ccbf6dc79040c9d3f0791e
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-09-13 00:43:15 +0530

    Update 12Sep23.cpp

--- September2023/12Sep23.cpp
+++ September2023/12Sep23.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+      map<char,int>mp;
+      for(auto it:s)
+      {
+          mp[it]++;
+    
+      }  
+      unordered_set<int>freq;
+      int ans=0;
 
+      for(auto it:mp)
+      {
+          int f=it.second;
+          while(f>0&&freq.find(f)!=freq.end())
+          {
+              f--;ans++;
+          }
+          freq.insert(f);
+
+      }
+      return ans;
+     
+    }
+};
