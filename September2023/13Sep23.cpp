2b924be1fe143b04869eb025822e9206b1414f97
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-09-13 23:33:14 +0530

    Update 13Sep23.cpp

--- September2023/13Sep23.cpp
+++ September2023/13Sep23.cpp
@@ -1,4 +1,9 @@
-class Solution {
+73. Set Matrix Zeroes
+
+Hint
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.class Solution {
 public:
     void change(vector<vector<int>>& matrix,int n,int m,int i,int j)
     {
