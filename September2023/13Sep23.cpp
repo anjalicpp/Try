221604eebe23902ebded4bd6744f6b6a14771664
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-09-13 23:32:48 +0530

    Update 13Sep23.cpp

--- September2023/13Sep23.cpp
+++ September2023/13Sep23.cpp
@@ -1,1 +1,49 @@
+class Solution {
+public:
+    void change(vector<vector<int>>& matrix,int n,int m,int i,int j)
+    {
+      for(int a=0;a<m;a++)
+      {
+        if(matrix[i][a]!=0)
+        {
+          matrix[i][a]=INT_MIN+2;
+        }
 
+        for(int b=0;b<n;b++)
+        {
+          if(matrix[b][j]!=0)
+          {
+            matrix[b][j]=INT_MIN+2;
+          }
+        }
+      }
+    }
+    void setZeroes(vector<vector<int>>& matrix) {
+      int n=matrix.size();
+      int m=matrix[0].size();
+
+       for(int i=0;i<matrix.size();i++)
+       {
+         for(int j=0;j<matrix[0].size();j++)
+         {
+           if(matrix[i][j]==0)
+           {
+             change(matrix,n,m,i,j);
+           }
+         }
+       }
+//changing the matrix
+       for(int i=0;i<n;i++)
+       {
+         for(int j=0;j<m;j++)
+         {
+           if(matrix[i][j]==INT_MIN+2)
+           {
+             matrix[i][j]=0;
+           }
+         }
+       }
+
+
+    }
+};
