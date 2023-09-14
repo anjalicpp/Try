e62d9072dd4cdd5730537b126f3b2c4b5c084c74
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-09-14 21:56:58 +0530

    Update 14Sep23.cpp

--- September2023/14Sep23.cpp
+++ September2023/14Sep23.cpp
@@ -1,1 +1,28 @@
+class OrderedStream {
+public:map<int,string>mp;
+int count=1;
+    OrderedStream(int n) {
+        
+    }
+    
+    vector<string> insert(int idKey, string value) {
+        mp.insert({idKey,value});
+        vector<string> ans;
 
+        for(auto it:mp)
+        {
+            if(it.first==count)
+           { ans.push_back(it.second);
+            count++;
+           }
+        }
+       
+        return ans;
+    }
+};
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * OrderedStream* obj = new OrderedStream(n);
+ * vector<string> param_1 = obj->insert(idKey,value);
+ */
