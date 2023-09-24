c7d7754c2a10a86b93bd64690a8a1936040660a1
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-09-24 19:53:19 +0530

    Update 24Sep23.cpp

--- September2023/24Sep23.cpp
+++ September2023/24Sep23.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+      unordered_map<string,int>mpp;
+        for(auto &s : arr){
+            mpp[s]++;
+        }
+        int cnt=0;
+        for (auto &s : arr){
+            if(mpp[s]==1){
+                cnt++;
+                if(cnt==k){
+                    return s;
+                }
+            }
+        }
+        return "";   
+    }
+};
