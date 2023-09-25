d066498dda8988f41c80f178727b9c8bd9e66998
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-09-25 21:19:32 +0530

    Update 25Sep23.cpp

--- September2023/25Sep23.cpp
+++ September2023/25Sep23.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+      /*  int res=0;
+      for(auto i:s)res=i^res;
+       for(auto i:t)res=i^res;
+       return res;*///bitwise approach
 
+       map<char,int>m;
+       for(auto i:s)
+       {
+           m[i]++;
+       }
+       for(auto i:t)
+       {
+           
+           if(--m[i]<0)return i;
+            
+       }
+return'\0';
+    }
+};
