7862edf4cd50d06edc3ee7e2c3b862a313712856
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-09-26 22:44:46 +0530

    Update 26Sep23.cpp

--- September2023/26Sep23.cpp
+++ September2023/26Sep23.cpp
@@ -1,1 +1,42 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char,int>mp;//keep account of last occurance of index of each character
+        unordered_set<char>vis;//keep cout for visited ones
 
+        for(int i=0;i<s.length();i++)
+        {
+            mp[s[i]]=i;
+        }
+
+        stack<char>st;
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(vis.find(s[i])!=vis.end())
+            {
+                //if its already visited skip it
+                continue;
+            }
+
+            while(!st.empty()&&s[i]<st.top()&&i<mp[st.top()])
+            {
+                //erase it from stack as well as visited 
+                vis.erase(st.top());
+                st.pop();
+    
+            }
+            vis.insert(s[i]);
+            st.push(s[i]);
+        }
+
+            string ans="";
+            while(!st.empty())
+            {
+                ans=st.top()+ans;
+                st.pop();
+            }
+        
+    return ans;
+    }
+};
