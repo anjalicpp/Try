a740ccbd3aaaefd78af19dedf293e3e2cda9d60f
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-10-05 22:01:32 +0530

    Update 5Oct23.cpp

--- October2023/5Oct23.cpp
+++ October2023/5Oct23.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int limit=floor(nums.size()/3);
+        vector<int> ans;
+       map<int,int>mp;
+      for(auto it:nums)
+      {
+          mp[it]++;
+      } 
+      priority_queue<pair<int,int>>mh;
+      for(auto i:mp)
+      {
+          mh.push({i.second,i.first});
+      }
+        while(mh.size()>0)
+        {
+            if(mh.top().first>limit)
+            ans.push_back(mh.top().second);
+            mh.pop();
+        } 
+        return ans;
+    }
+};
