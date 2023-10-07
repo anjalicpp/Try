2ec003ed31664967b822b6a2a2c91febcc0487a6
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-10-07 23:19:55 +0530

    Update 7Sep23.cpp

--- October2023/7Sep23.cpp
+++ October2023/7Sep23.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+       int i=0,j=0;
+       int count=0;
+       map<char,int> mp;
+       while(j<s.size())
+       {
+           mp[s[j]]++;
+           if(j-i+1<3)
+           {
+               j++;
+           }
+           else if(j-i+1==3)
+           {
+            if(mp.size()==3)
+            {
+                count++;
+            }
+            
+            mp[s[i]]--;
+            if(mp[s[i]]==0)
+            {
+                mp.erase(s[i]);
+            }
+            i++;
+            j++;
 
+           }
+
+          
+       } 
+       return count;
+    }
+};
