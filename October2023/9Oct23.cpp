8ea14520a49802459a3b07360c3be56d153fd7ae
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-10-09 12:32:44 +0530

    Update 9Oct23.cpp

--- October2023/9Oct23.cpp
+++ October2023/9Oct23.cpp
@@ -1,1 +1,65 @@
+//{ Driver Code Starts
+//Initial template for C++
 
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+}; 
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        // code here 
+        if(node==NULL)
+        {
+            return 0;
+        }
+        
+        else 
+        return 1+max(height(node->right),height(node->left));
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string treeString;
+		getline(cin,treeString);
+		Node* root = buildTree(treeString);
+        Solution ob;
+		cout<<ob.height(root)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
