1468b46ea61ee952977cf48783bfc0312a9d72eb
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-10-11 21:11:11 +0530

    Update 11Oct23.cpp

--- October2023/11Oct23.cpp
+++ October2023/11Oct23.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<int>g;
+        int l=0,r=nums.size();int mid=l+r/2;
+       for(int i=0;i<nums.size();i++)
+       {
+           if(nums[i]==target)
+               g.push_back(i);
+       }
+        return g;
+    }
+};
