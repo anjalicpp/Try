a8f3777e2559101b57b0b038845d54c5b33fc64b
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-10-13 21:29:53 +0530

    Update 13Oct23.cpp

--- October2023/13Oct23.cpp
+++ October2023/13Oct23.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    string reverseWords(string s) {
+        int i=0;
+        int j;
+        for(j=0;j<s.size();j++)
+        {
+            if(s[j]==' ')
+            {
+                reverse(s.begin()+i,s.begin()+j);
+                i=j+1;
+            }
+        }
+        reverse(s.begin()+i,s.end());
+        return s;
+        
+    }
+};
