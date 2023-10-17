c269d582e7068556966bb10d4708c663a326838d
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-10-17 23:09:42 +0530

    Update 17Oct23.cpp

--- October2023/17Oct23.cpp
+++ October2023/17Oct23.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+    int rem=0;
+    int flag=n%2;
+    while(n)
+    {
+        rem=n%2;
+        if(rem!=flag)return false;
+        n=n/2;
+        flag=!flag;
 
+    }
+     return true;  
+    }
+};
