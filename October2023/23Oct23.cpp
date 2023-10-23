f1cfc43a33408a4d660cbfebe626a83150cf4faf
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-10-23 20:37:38 +0530

    Update 23Oct23.cpp

--- October2023/23Oct23.cpp
+++ October2023/23Oct23.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==1)return true;
+        
+        else if(n%4!=0||n==0)
+        {
+            return false;
+        }
+        return isPowerOfFour(n/4);
+        
+        
+    }
+};
