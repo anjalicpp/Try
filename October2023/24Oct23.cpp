5a3efecd337240390bb5c95fcc9f3442c8cc7787
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-10-24 20:59:42 +0530

    Update 24Oct23.cpp

--- October2023/24Oct23.cpp
+++ October2023/24Oct23.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if(root==NULL)return{};
+        queue<TreeNode*> q;
+        vector<int>ans;
 
+
+        q.push(root);
+        
+
+        while(!q.empty())
+        {
+           int size=q.size();
+           int maxi=INT_MIN;
+           for(int i=0;i<size;i++)
+           {
+               TreeNode*node=q.front();
+               q.pop();
+               if(node->left)q.push(node->left);
+               if(node->right)q.push(node->right);
+               vector<int>temp;
+               maxi=max(maxi,node->val);
+
+           }
+           ans.push_back(maxi);
+        }
+        return ans;    
+    }
+};
