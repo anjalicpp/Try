d1cc572edcc775c672b5dbd70b37bf32452bf926
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-11-01 23:27:32 +0530

    Update 1Nov23.cpp

--- November2023/1Nov23.cpp
+++ November2023/1Nov23.cpp
@@ -1,1 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
 
+    void solve(TreeNode*root,map<int,int>&mp)
+    {
+        if(root==NULL)return;
+
+        solve(root->left,mp);
+        mp[root->val]++;
+        solve(root->right,mp);
+    }
+    vector<int> findMode(TreeNode* root) {
+      vector<int> ans;
+      map<int,int>mp;
+      solve(root,mp); 
+
+      int Maxfreq=0;
+      for(auto it:mp)
+      {
+          if(it.second>Maxfreq)
+          {
+              ans.clear();
+              ans.push_back(it.first);
+              Maxfreq=it.second;
+          }
+          else if(Maxfreq==it.second)
+          {
+              ans.push_back(it.first);
+          }
+      } 
+      return ans;
+    }
+};
