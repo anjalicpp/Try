1d26884b986da44e6b1371b59a5cde2491ee0929
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-11-08 13:19:08 +0530

    Update 8Nov23.cpp

--- November2023/8Nov23.cpp
+++ November2023/8Nov23.cpp
@@ -11,3 +11,14 @@ public:
     }
 }; 
 
+Intuition:
+In this problem, we are given a cell grid with a source cell (sx, sy), a target cell (fx, fy), and a time limit 't'. 
+The task is to determine whether it's possible to reach the target cell from the source cell within exactly 't' units of time. 
+To solve this, we need to consider the maximum distance between the source and target cells and compare it to the given time limit.
+
+Explanation:
+If the source and target cells are the same, it checks if 't' is not equal to 1.
+This is because you need at least one unit of time to move between them, so 't' must be greater than 1 in this case.
+If the source and target cells are different, it calculates the maximum horizontal or vertical distance between them.
+It checks if this maximum distance is less than or equal to 't'.
+If yes, it's possible to reach the target cell within 't' units of time.
