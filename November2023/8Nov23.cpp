02728f59125b718a68ac9c5ab99b11458b0c4c8e
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-11-08 13:15:56 +0530

    Update 8Nov23.cpp

--- November2023/8Nov23.cpp
+++ November2023/8Nov23.cpp
@@ -1,1 +1,12 @@
+class Solution {
+public:
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        if((sx==fx)&&(sy==fy)) return t!=1;
+       
+       int xdist=abs(sx-fx);
+       int ydist=abs(sy-fy);
 
+       int mindist=min(xdist,ydist)+abs(ydist-xdist);
+       return t>=mindist;
+    }
+}; 
