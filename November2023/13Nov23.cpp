7c3eedd0d5f0f20c8d7eeea7573b0ea0c59d98d8
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-11-13 20:18:20 +0530

    Update 13Nov23.cpp

--- November2023/13Nov23.cpp
+++ November2023/13Nov23.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+     string st="";
+     for(auto i:s)
+     {
+         if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u'||i=='A'||i=='E'||i=='I'||i=='O'||i=='U')
+         st+=i;
+     } 
 
+     //sorting the array of strings  
+     sort(st.begin(),st.end());
+    
+
+     //replacing character in strings from the sorted string st
+     int j=0;
+     for(int i=0;i<s.size();i++)
+     {
+         if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+        {
+            s[i]=st[j];
+            j++;
+        }
+     }
+     return s;
+    }
+};
