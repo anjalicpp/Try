f05159e7b26c4df32f82db84597621c2727f76d2
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-11-15 16:16:54 +0530

    Update 15Nov23.cpp

--- November2023/15Nov23.cpp
+++ November2023/15Nov23.cpp
@@ -1,1 +1,19 @@
+1846. Maximum Element After Decreasing and Rearranging
 
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+       sort(arr.begin(),arr.end());
+       arr[0]=1;//condition in question
+
+       for(int i=1;i<arr.size();i++)
+       {
+           if(abs(arr[i]-arr[i-1])<=1)continue;//if satisfy move on
+           else arr[i]=arr[i-1]+1;//if not satisfied make element previous+1;
+       }
+       return arr[arr.size()-1];
+
+
+
+    }
+};
