8eb7f47ec5660d6fec0810cdcfb98e619a9251a7
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-11-17 22:58:40 +0530

    Update 17Nov23.cpp

--- November2023/17Nov23.cpp
+++ November2023/17Nov23.cpp
@@ -1,1 +1,22 @@
+1877. Minimize Maximum Pair Sum in Array
 
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        //sorting array so that we can make pairs using two pinter with highest&lowest pairing
+
+        int ans=0;
+        int i=0;
+        int j=nums.size()-1;
+        //using two pointer approach
+        while(i<j)
+        {
+            ans=max(ans,nums[i]+nums[j]);
+           i++;
+           j--;
+        }
+
+        return ans;
+    }
+};
