3ed8b15f699e5bfc7ee01404203e2b62192e0069
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-11-22 22:09:31 +0530

    Update 22Nov23.cpp

--- November2023/22Nov23.cpp
+++ November2023/22Nov23.cpp
@@ -1,1 +1,37 @@
 
+class Solution {
+public:
+    int rev_num(int num)
+    {
+        int rev=0;
+        int rem=0;
+        while(num)
+        {
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        return rev;
+    }
+
+    int countNicePairs(vector<int>& nums) {
+
+      map<int,int>mp;
+      int count=0;
+
+      for(int i=0;i<nums.size();i++)
+      {
+          nums[i]=nums[i]-rev_num(nums[i]);
+          //made condition better by rearranging:nums[i]-rev(nums[i])==nums[j]-rev(nums[j])
+          if(mp.find(nums[i])!=mp.end())
+          {
+            //if found in map then increasing counter before adding to map
+             count+=mp[nums[i]];
+             count=count%1000000007;
+          }
+          mp[nums[i]]++;
+       } 
+    
+     return count%1000000007;
+    }
+};
