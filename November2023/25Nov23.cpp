9eca6f8a239d21a676a0790222db18cf964efb88
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-11-26 00:07:33 +0530

    Update 25Nov23.cpp

--- November2023/25Nov23.cpp
+++ November2023/25Nov23.cpp
@@ -1,1 +1,21 @@
+Brute force :42/59 cases
 
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        vector<int>res;
+
+        for(int)
+
+       for(int i=0;i<nums.size();i++)
+       {
+           int sum=0;
+           for(int j=0;j<nums.size();j++)
+           {
+               sum+=abs(nums[j]-nums[i]);
+           }
+           res.push_back(sum);
+       } 
+       return res;
+    }
+};
