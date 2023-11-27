4166f15fa8caf3b95bf49649ae60581b09e0f041
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-11-27 21:47:56 +0530

    Update 27Nov23.cpp

--- November2023/27Nov23.cpp
+++ November2023/27Nov23.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+      vector<int> ans;
+      
 
+      /*for(int i=0;i<words.size();i++)
+      {
+          for(int j=0;j<words[i].length();j++)
+          {
+              if(words[i][j]==x)
+              {
+                 ans.push_back(i);
+                 break; 
+              }
+            
+          }
+        
+      } 
+      return ans; */
+-------------------------------------------------------------------------
+      int count=0;
+      for(auto st:words)
+      {
+          for(auto it:st)
+          {
+              if(it==x)
+              {
+                  ans.push_back(count);break;
+              }
+          }
+          count++;
+      }
+      return ans;
+    }
+};
