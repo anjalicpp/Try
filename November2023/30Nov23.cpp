55b0cccd21beefbc3956fc41c37dd2cb5b931ee5
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-11-30 23:06:30 +0530

    Update 30Nov23.cpp

--- November2023/30Nov23.cpp
+++ November2023/30Nov23.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int findMinimumOperations(string s1, string s2, string s3) {
+     int size=s1.length();
+     if(size>s2.length())
+     {
+         size=s2.length();
+     }
+     if(size>s3.length())
+     {
+         size=s3.size();
+     }
+        int i=0; 
 
+        while(i<size)
+        {
+            if(s1[i]!=s2[i]||s2[i]!=s3[i]||s1[i]!=s3[i])break;
+            else i++;
+        } 
+        if(i==0)return -1;
+        else return i;
+    }
+};
