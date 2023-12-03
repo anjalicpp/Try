aaad1aa3d6c98fefc3a38af873607def812d0ed4
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-03 20:08:03 +0530

    Update 3Dec23.cpp

--- December2023/3Dec23.cpp
+++ December2023/3Dec23.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+       int ans=0;
 
+      for(int i=0;i<points.size()-1;i++)
+      {
+          int y_res=abs(points[i+1][1]-points[i][1]);
+          int x_res=abs(points[i+1][0]-points[i][0]);
+          ans+=max(x_res,y_res);
+      }  
+      return ans;
+    }
+};
