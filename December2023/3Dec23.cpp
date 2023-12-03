0c0b080703078c01ca1fe6994e946bf66badff05
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-03 20:09:58 +0530

    Update 3Dec23.cpp

--- December2023/3Dec23.cpp
+++ December2023/3Dec23.cpp
@@ -1,3 +1,5 @@
+1266. Minimum Time Visiting All Points
+
 class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
@@ -5,8 +7,8 @@ public:
 
       for(int i=0;i<points.size()-1;i++)
       {
-          int y_res=abs(points[i+1][1]-points[i][1]);
-          int x_res=abs(points[i+1][0]-points[i][0]);
+          int y_res=abs(points[i+1][1]-points[i][1]);//finding y coordinate from two points
+          int x_res=abs(points[i+1][0]-points[i][0]);//finding x coordinates from two points
           ans+=max(x_res,y_res);
       }  
       return ans;
