e9153d6a58fd72e4f1758a7e49f93af0ad419f6f
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-10 22:08:06 +0530

    Update 10Dec23.cpp

--- December2023/10Dec23.cpp
+++ December2023/10Dec23.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+    int n=matrix.size();
+    int m=matrix[0].size();
 
+   /* vector<vector<int>>ans(m,vector<int>(n,0));
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            ans[j][i]=matrix[i][j];
+        }
+    }
+
+    return ans;*/
+    vector<vector<int>>ans;
+
+    for(int i=0;i<m;i++)
+    {
+        vector<int>temp;
+        for(int j=0;j<n;j++)
+        {
+            temp.push_back(matrix[j][i]);
+        }
+        ans.push_back(temp);
+    }
+    return ans;
+    }
+};
