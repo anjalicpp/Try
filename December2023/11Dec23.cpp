3156e44380c77352124ca98a9dfd3decbc47a08b
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-11 08:34:51 +0530

    Update 11Dec23.cpp

--- December2023/11Dec23.cpp
+++ December2023/11Dec23.cpp
@@ -1,1 +1,17 @@
+1287. Element Appearing More Than 25% In Sorted Array
 
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+       int times=0.25*arr.size();
+       map<int,int>mp;
+
+      for(int i=0;i<arr.size();i++)
+      {
+          mp[arr[i]]++;
+          if(mp[arr[i]]>times)
+          return arr[i];
+      }
+       return 0;
+    }
+};
