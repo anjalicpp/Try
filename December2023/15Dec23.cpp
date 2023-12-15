1f6927f001b0983e36e704c99d1a62b347f87d21
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-15 20:27:26 +0530

    Update 15Dec23.cpp

--- December2023/15Dec23.cpp
+++ December2023/15Dec23.cpp
@@ -1,1 +1,29 @@
+1436. Destination City
 
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        //logic:put in map and count occcurance only destination has 1 count
+        map<string,int>mp;
+        string ans="";
+        int k=1;
+
+        for(auto it:paths)
+        {
+            for(auto id:it)
+           {
+               mp[id]=mp[id]+k;
+               k=-k;
+           }
+
+        }
+        for(auto it:mp)
+        {
+            if(it.second==-1)
+            {
+                ans+=it.first;
+            }
+        }
+        return ans;
+    }
+};
