1e0654863fa9d13c813e60c018c3dbda49537843
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-16 08:51:19 +0530

    Update 16Dec23.cpp

--- December2023/16Dec23.cpp
+++ December2023/16Dec23.cpp
@@ -1,1 +1,29 @@
+242. Valid Anagram
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       map<char,int>mp;
+       for(auto it:s)
+       {
+           mp[it]++;
+       }
 
+       int i=0;
+       int count=mp.size();
+       while(i<t.size())
+       {
+           if(mp.find(t[i])==mp.end())
+           {
+               return false;
+           }
+           else {
+               mp[t[i]]--;
+               if(mp[t[i]]==0)
+               count--;
+           }
+           i++;
+       }
+       if(count==0)return true;
+       else return false;
+    }
+};
