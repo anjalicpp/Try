5792dc67a7ee01578e03b728e2141a5587961c22
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-20 22:07:05 +0530

    Update 20Dec23.cpp

--- December2023/20Dec23.cpp
+++ December2023/20Dec23.cpp
@@ -1,1 +1,10 @@
-
+2706. Buy Two Chocolates
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(),prices.end());
+        int ans=money-(prices[0]+prices[1]);
+        if(ans<0)return money;
+        else return ans;
+    }
+};
