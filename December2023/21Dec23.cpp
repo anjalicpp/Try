758bf4bfcfb69e8084209c1c64e7770655bde0da
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-21 22:04:40 +0530

    Update 21Dec23.cpp

--- December2023/21Dec23.cpp
+++ December2023/21Dec23.cpp
@@ -1,1 +1,20 @@
+1637. Widest Vertical Area Between Two Points Containing No Points
+class Solution {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+      //do it yourself-->Streak
+       vector<int> xSorted;
+        for (const auto& point : points) {
+            xSorted.push_back(point[0]);
+        }
 
+        sort(xSorted.begin(), xSorted.end());
+
+        int maxWidth = 0;
+        for (int i = 0; i < xSorted.size() - 1; i++) {
+            maxWidth = max(maxWidth, xSorted[i + 1] - xSorted[i]);
+        }
+
+        return maxWidth;  
+    }
+};
