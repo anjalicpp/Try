a4248a55966f92d81979054f5608347bbdd067b6
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-23 22:47:27 +0530

    Update 23Dec23.cpp

--- December2023/23Dec23.cpp
+++ December2023/23Dec23.cpp
@@ -1,1 +1,49 @@
 1496. Path Crossing
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+       /* unordered_set<string> points;
+        points.insert("0,0");
+        int x = 0, y = 0;
+
+        for (char d : path) {
+            if (d == 'N') y++;
+            else if (d == 'S') y--;
+            else if (d == 'E') x++;
+            else if (d == 'W') x--;
+
+            if (points.count(to_string(x) + "," + to_string(y))) return true;
+            points.insert(to_string(x) + "," + to_string(y));
+        }
+
+        return false;  */
+         int a = 0, b = 0;;
+        set<vector<int>> x;
+        x.insert({0, 0});
+        for (char i: path)
+        {
+            if (i == 'N')
+            {
+                b++;
+            } 
+            else if (i == 'E')
+            {
+                a++;
+            } 
+            else if (i == 'S')
+            {
+                b--;
+            }
+            else 
+            {
+                a--;
+            }
+            if (x.find({a, b}) != x.end())
+            {
+                return true;
+            }
+            x.insert({a, b});
+        }
+        return false;
+    }
+};
