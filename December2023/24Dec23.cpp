dc0120312ad56c15e69f5564c2c26d61d01c795b
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-24 19:58:32 +0530

    Update 24Dec23.cpp

--- December2023/24Dec23.cpp
+++ December2023/24Dec23.cpp
@@ -1,1 +1,27 @@
+1758. Minimum Changes To Make Alternating Binary String
+class Solution {
+public:
+    int minOperations(string s) {
+     int zeroOnEven = 0;//size=4    1010   0101
+        int zeroOnOdd  = 0;
 
+        int oneOnEven = 0;
+        int oneOnOdd  = 0;
+
+        for( int i = 0 ; i < s.length(); i++)
+        {
+            if( i % 2 == 0)
+            {
+                if(s[i] == '0')oneOnEven += 1;
+                else zeroOnEven += 1;
+            }
+            else
+            {
+                if(s[i] == '0') oneOnOdd += 1;
+                else  zeroOnOdd += 1;
+            }
+        }
+
+        return min( zeroOnEven + oneOnOdd , oneOnEven + zeroOnOdd);
+    }
+};
