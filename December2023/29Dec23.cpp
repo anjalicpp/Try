053b00840b917b350304ce2fa81ff08bb3a49319
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-29 20:07:03 +0530

    Update 29Dec23.cpp

--- December2023/29Dec23.cpp
+++ December2023/29Dec23.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public://Streak----only------>
+     int dp[300][1001];
+    int f(int idx,int d,vector<int> &jobDifficulty) {
+        //base cases
+        if(idx == jobDifficulty.size() && d == 0) return 0;
+        if(idx == jobDifficulty.size() || d == 0 || jobDifficulty.size() < d+idx) return 1e9;
+        if(dp[idx][d] != -1) return dp[idx][d];
+        
+
+        int maxi=0,ans=1e9;
+        for(int i=idx;i<jobDifficulty.size();++i) {
+            maxi=max(maxi,jobDifficulty[i]);
+            int res=f(i+1,d-1,jobDifficulty);
+            if(res!=1e9) ans=min(ans,maxi+res);
+        }
+        return dp[idx][d]=ans;
+    }
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        //edge case
+        if(jobDifficulty.size() < d) return -1;
+        memset(dp,-1,sizeof(dp));
+        return f(0,d,jobDifficulty);
+        
+    }
+};
 2
