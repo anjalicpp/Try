3bcdd0e683c391971ecb91ed8b82e4a9042d213e
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-30 20:03:45 +0530

    Update 30Dec23.cpp

--- December2023/30Dec23.cpp
+++ December2023/30Dec23.cpp
@@ -1,1 +1,24 @@
+1897. Redistribute Characters to Make All Strings Equal
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+      int n=words.size();
+      unordered_map<char,int>mp;
 
+      for(auto it:words)
+      {
+          for(auto i:it)
+          {
+              mp[i]++;//keeping count of each character
+          }
+      }  
+
+      for(auto it:mp)
+      {
+          if(it.second%n!=0)
+          //checking if no of character is divided by size of words==true else false
+          return false;
+      }
+      return true;
+    }
+};
