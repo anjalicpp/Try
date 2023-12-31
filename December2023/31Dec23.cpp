df41af7132e74184027382518f7a985133e91389
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-12-31 20:12:05 +0530

    Update 31Dec23.cpp

--- December2023/31Dec23.cpp
+++ December2023/31Dec23.cpp
@@ -1,1 +1,21 @@
+1624. Largest Substring Between Two Equal Characters
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+       map<char,pair<int,int>>mp;//map<char,pair<first_occurance,last_occurance>>
+       int ans=-1;
 
+       for(int i=0;i<s.length();i++)
+       {
+           if(mp.find(s[i])!=mp.end())
+           {
+               mp[s[i]].second=i;
+           }
+           else{
+               mp[s[i]].first=i;
+           }
+           ans=max(mp[s[i]].second-mp[s[i]].first-1,ans);
+       } 
+       return ans;
+    }
+};
