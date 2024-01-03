81e8990d3dc64c52ac9abc662132d00cb58de862
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-01-03 23:18:17 +0530

    Update 3Jan23.cpp

--- Code_2024/Jan2024/3Jan23.cpp
+++ Code_2024/Jan2024/3Jan23.cpp
@@ -1,1 +1,13 @@
-
+2980. Check if Bitwise OR Has Trailing Zeros
+class Solution {
+public:
+    bool hasTrailingZeros(vector<int>& nums) {
+        int count=0;
+        for(auto it:nums)
+        {
+            if(it%2==0)count++;
+            if(count>=2)return true;
+        }
+        return false;
+    }
+};
