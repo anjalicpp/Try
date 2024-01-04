bdca97d2df09791f326892c050b6f6cc3ebaab31
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-01-04 23:10:25 +0530

    Update 4Jan23.cpp

--- Code_2024/Jan2024/4Jan23.cpp
+++ Code_2024/Jan2024/4Jan23.cpp
@@ -1,1 +1,25 @@
+2870. Minimum Number of Operations to Make Array Empty
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+         unordered_map<int,int>mp;
+       for(auto i:nums){
+           mp[i]++;
+       } 
+       int ans=0;
 
+       for(auto i:mp){
+           if(i.second<2) return -1;
+           else if(i.second==2 || i.second==3) ans++;
+           else if(i.second>3){
+               int count=0;
+               int rem=0;
+               count=i.second/3;
+               rem=i.second%3;
+               ans+=count;
+               if(rem!=0) ans++;
+           }
+       }
+       return ans;
+    }
+};
