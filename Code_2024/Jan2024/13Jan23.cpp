f1e44b584c8c8fc0aa8a7f5f93ebb3a7dda34b58
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-01-13 14:44:22 +0530

    Update 13Jan23.cpp

--- Code_2024/Jan2024/13Jan23.cpp
+++ Code_2024/Jan2024/13Jan23.cpp
@@ -1,1 +1,31 @@
+1347. Minimum Number of Steps to Make Two Strings Anagram
 
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        map<char,int>mp;
+        for(auto it:s)
+        {
+            mp[it]++;//keeping count of frequencies in s
+        
+        }
+        int ans=0;
+
+        for(auto it:t)
+        {
+            if(mp.find(it)==mp.end())//checking corresponding charcters in t
+            {
+                ans++;
+            }
+            else
+            {
+                mp[it]--;
+                if(mp[it]==0)
+                {
+                    mp.erase(it);
+                }
+            }
+        }
+        return ans;
+    }
+};
