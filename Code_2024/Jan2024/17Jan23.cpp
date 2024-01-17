f343b3d4e4eb24ec5ffc56d5fecab99ff8615f09
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-01-17 22:46:50 +0530

    Update 17Jan23.cpp

--- Code_2024/Jan2024/17Jan23.cpp
+++ Code_2024/Jan2024/17Jan23.cpp
@@ -1,1 +1,17 @@
 
+1207. Unique Number of Occurrencesclass 
+Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+       unordered_map<int,int>freq;
+      for(auto x: arr){
+          freq[x]++;
+      }  
+      unordered_set<int>s;
+      for(auto x: freq){
+          s.insert(x.second);
+      }
+      return freq.size()==s.size();
+
+    }
+};
