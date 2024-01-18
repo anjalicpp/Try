dbb6e43169a8d93b56966a504c131b8307032c34
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-01-18 19:48:35 +0530

    Update 18Jan23.cpp

--- Code_2024/Jan2024/18Jan23.cpp
+++ Code_2024/Jan2024/18Jan23.cpp
@@ -1,1 +1,11 @@
-1
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0||n==1)
+         return 1;
+        
+         int ans = climbStairs(n-1)+ climbStairs(n-2);
+
+         return ans;
+    }
+};1
