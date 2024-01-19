f8355170bfa85435c898529dc73eba13b06e7622
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-01-19 23:20:43 +0530

    Update 19Jan23.cpp

--- Code_2024/Jan2024/19Jan23.cpp
+++ Code_2024/Jan2024/19Jan23.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+    unordered_map<int,int>mp;
+    int maxi=0;
+    for(auto it:nums)
+    {
+        mp[it]++;
+        maxi=max(mp[it],maxi);
+    }  
 
+     vector<vector<int>> ans(maxi);
+     
+
+    
+         
+         for(auto it:mp)
+         {
+           int num=it.first;
+           int freq=it.second;
+
+           for(int i=0;i<freq;i++)
+           {
+               ans[i].push_back(num);
+           }
+        
+         }
+         
+
+     return ans;
+    }
+};
