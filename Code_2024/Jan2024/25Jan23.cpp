1c950b09443bc2d3e96f41b15883fa4c83c5186a
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-01-25 21:23:31 +0530

    Update 25Jan23.cpp

--- Code_2024/Jan2024/25Jan23.cpp
+++ Code_2024/Jan2024/25Jan23.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    
+    bool check(char s)
+    {
+        if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u'||s=='A'||s=='E'||s=='I'||s=='O'||s=='U')
+        return true;
+        
+        else return false;
+    }
+    bool halvesAreAlike(string s) {
+    int size=s.size();
+        int vow1=0,vow2=0;
+        for(int i=0,j=size/2;i<size/2;i++,j++)
+        {
+            if(check(s[i]))
+                vow1++;
+            if(check(s[j]))
+                vow2++;
+            
+        }
+       return vow1==vow2; 
+    }
+};
