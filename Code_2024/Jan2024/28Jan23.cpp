ce1172f17523e9b94dcbea301840a6e29b3cf0a8
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-01-28 23:40:50 +0530

    Update 28Jan23.cpp

--- Code_2024/Jan2024/28Jan23.cpp
+++ Code_2024/Jan2024/28Jan23.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    string reverseWords(string s) {
+        int i=0;
+        int j;
+        for(j=0;j<s.size();j++)
+        {
+            if(s[j]==' ')
+            {
+                reverse(s.begin()+i,s.begin()+j);
+                i=j+1;
+            }
+        }
+        reverse(s.begin()+i,s.end());
+        return s;
+        
+    }
+};
