e9e873e403c21cb2d1cfcfa23999b0fcf099e8e1
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-01-31 21:15:47 +0530

    Update 31Jan23.cpp

--- Code_2024/Jan2024/31Jan23.cpp
+++ Code_2024/Jan2024/31Jan23.cpp
@@ -1,1 +1,50 @@
+class Solution {
+public:
+    void solve(vector<int> &v,int n,vector<int> &arr)
+    {
+        stack<pair<int,int>> s;
 
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s.size()==0)
+            {
+                v.push_back(-1);
+            }
+            else if(s.size()>0&&s.top().first>arr[i])
+            {
+                v.push_back(s.top().second);
+            }
+            else if(s.size()>0&&s.top().first<=arr[i])
+            {
+                while(s.size()>0&&s.top().first<=arr[i])
+                {
+                    s.pop();
+                }
+                if(s.size()==0)
+                {
+                    v.push_back(-1);
+                }
+                else v.push_back(s.top().second);
+            }
+            s.push({arr[i],i});
+        }
+
+        reverse(v.begin(),v.end());
+
+        for(int i=0;i<n;i++)
+        {
+            if(v[i]==-1)
+            v[i]=0;
+            else
+            v[i]=v[i]-i;
+        }
+
+    
+    }
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+      vector<int> ans;
+      int n=temperatures.size();
+      solve(ans,n,temperatures);
+      return ans;  
+    }
+};
