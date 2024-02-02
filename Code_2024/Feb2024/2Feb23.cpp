0689759cb2e657bcbf7e4f886296f3f88affafe2
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-02-02 19:04:19 +0530

    Update 2Feb23.cpp

--- Code_2024/Feb2024/2Feb23.cpp
+++ Code_2024/Feb2024/2Feb23.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<int> st;
+        if(s.size()==1)return false;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')//opening bracket ko push kro
+            {
+                st.push(s[i]);
+                
+            }
+            else{
+                if(st.empty()) return false;//stack empty wala case
+                
+                char c=st.top();
+                st.pop();
+                
+                if((c=='('&&s[i]!=')')||(c=='{'&&s[i]!='}')||(c=='['&&s[i]!=']'))
+                return false;
+            }
+            
+        }
+        if(st.empty())return true;
+        
+        else return false;
+        
+    }
+};
