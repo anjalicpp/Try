0b080269c6febf5a1bf28276b87710d69422393c
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-02-07 16:14:14 +0530

    Update 7Feb23.cpp

--- Code_2024/Feb2024/7Feb23.cpp
+++ Code_2024/Feb2024/7Feb23.cpp
@@ -1,1 +1,36 @@
-
+class Solution {
+public:
+    string frequencySort(string s) {
+        map<char,int>m;
+        int size=s.size();
+        for(int i=0;i<size;i++)
+        {
+            m[s[i]]++;
+        }
+      vector<pair<int,char>>v;
+        for(auto i:m)
+        {
+            v.push_back({i.second,i.first});
+            //vector me store karo data in freq:char form me
+            
+        }
+        
+        sort(v.begin(),v.end());//increasing orderv of frequency
+        string ans="";
+        
+        //storing in string in increasing order of frequency
+        for(auto i:v)
+        {
+            for(int j=0;j<i.first;j++)
+            {
+                ans+=i.second;
+                
+            }
+            
+        }
+        //now sort bcz ans is in increasing order of frequency;
+        
+       reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
