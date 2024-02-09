d5af011a2358b8c86f5b6590d562d06bd4d0235e
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-02-09 19:55:01 +0530

    Update 9Feb23.cpp

--- Code_2024/Feb2024/9Feb23.cpp
+++ Code_2024/Feb2024/9Feb23.cpp
@@ -1,1 +1,45 @@
-
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+      /* for(int i=m;i<m+n;i++)
+       {
+          nums1[i]=nums2[i-m];
+             
+       }
+        sort(nums1.begin(),nums1.begin()+m+n);*/
+        
+        vector<int>v;
+        int i=0,j=0,k=0;
+        while(i<m&&j<n)
+        {
+            if(nums1[i]>nums2[j])
+            {
+                v.push_back(nums2[j]);
+                j++;
+            }
+            else
+            {
+                v.push_back(nums1[i]);
+            i++;
+                
+            }
+            
+        }
+        while(i<m)
+        {
+            v.push_back(nums1[i]);
+            i++;
+            
+        }
+    
+        while(j<n)
+        {
+            v.push_back(nums2[j]);
+            j++;
+            
+        }
+        nums1=v;
+    
+    }
+};
