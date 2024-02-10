44f11369d1c971a363f54f5a37c2cd02b48ba409
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-02-10 22:11:51 +0530

    Update 10Feb23.cpp

--- Code_2024/Feb2024/10Feb23.cpp
+++ Code_2024/Feb2024/10Feb23.cpp
@@ -1,1 +1,33 @@
-
+if(head==NULL)  //head is null case
+        {
+            return NULL;
+        }
+        
+        //if all value are same case from head
+        while(head!=NULL&&head->val==val)
+        {
+            ListNode* temp=head;
+            head=head->next;
+            delete temp;
+        }
+        
+       
+        ListNode*temp=head;
+         ListNode*prev=NULL;
+        while(temp!=NULL)
+        {
+            
+            if(temp->val==val)
+            {
+              prev->next=temp->next;
+                
+            }
+            else
+            {
+              prev=temp;
+            
+            }
+            temp=temp->next;
+             
+        }
+        return head;
