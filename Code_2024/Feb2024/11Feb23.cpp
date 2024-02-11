ffc28c16c45c55fdf10cb10d459144ff50e2addc
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-02-11 22:17:24 +0530

    Update 11Feb23.cpp

--- Code_2024/Feb2024/11Feb23.cpp
+++ Code_2024/Feb2024/11Feb23.cpp
@@ -1,1 +1,28 @@
-
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
+     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> mh;
+        vector<int> ans;
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+       
+       for(auto it:mp)
+       {
+           mh.push({it.second,it.first});
+           if(mh.size()>k)
+           {
+               mh.pop();
+           }
+       }
+       while(mh.size()>0)
+       {
+           ans.push_back(mh.top().second);
+           mh.pop();
+       }
+        return ans;
+    
+}
