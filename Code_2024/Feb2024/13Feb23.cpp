ebc5242a6a65178b2bc34eeba463bd90a0ef94de
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-02-13 21:22:24 +0530

    Update 13Feb23.cpp

--- Code_2024/Feb2024/13Feb23.cpp
+++ Code_2024/Feb2024/13Feb23.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+      string ans="";
+        int flag=0;
+        
+        for(auto s:words)
+        {
+            int l=0;
+            int h=s.length()-1;
+            while(l<=h)
+            {
+               if( s[l]!=s[h]){ flag=0;break;}
+                flag=1;l++;h--;
+            }
+            if(flag){ans+=s;return ans;}
+        }
+      return ans;   
+    }
+};
