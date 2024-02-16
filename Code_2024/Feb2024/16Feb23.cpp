ad031676911e8e2990476239fd4df3c8d2462e8b
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-02-16 21:09:18 +0530

    Update 16Feb23.cpp

--- Code_2024/Feb2024/16Feb23.cpp
+++ Code_2024/Feb2024/16Feb23.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+      map<int,int>mp;
 
+      for(auto it:arr)
+      {
+          mp[it]++;//eeping cout of frequency
+      } 
+
+      vector<int>freq;
+      for(auto it:mp)
+      {
+          freq.push_back(it.second);
+
+      } 
+      sort(freq.begin(),freq.end());
+
+      int count=0;
+      for(auto i:freq)
+      {
+          if(k>=i)
+          {
+              k=k-i;count++;
+          }
+          else {
+              break;//we cannot let k be negative
+          }
+      }
+      return freq.size()-count;
+    }
+};
