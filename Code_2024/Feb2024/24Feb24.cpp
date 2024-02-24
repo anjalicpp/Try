24c3886f5dc032f3e7022515b10633cc97d20209
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-02-24 21:26:04 +0530

    Update 24Feb24.cpp

--- Code_2024/Feb2024/24Feb24.cpp
+++ Code_2024/Feb2024/24Feb24.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        map<int,int>mp;
+        for(auto it:trust)
+        {
+            mp[it[1]]++;
+        }
 
+        for(auto it:mp)
+        {
+            if(it.second==n-1&&find(trust.begin(),trust.end())return it.first;
+        }
+        return -1;
+    }
+};
