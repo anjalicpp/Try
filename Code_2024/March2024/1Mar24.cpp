39064d2831cfd7bf2972a5e0eff7aa6d26a50080
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-03-01 20:24:15 +0530

    Update 1Mar24.cpp

--- Code_2024/March2024/1Mar24.cpp
+++ Code_2024/March2024/1Mar24.cpp
@@ -1,1 +1,28 @@
+2864. Maximum Odd Binary Number
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+      int ones=0,zeros=0;
+      string ans="";
 
+      for(auto it:s)
+      {
+          if(it=='0')zeros++;
+          else ones++;
+      }  
+        
+        ones--;
+        
+      while(ones--)
+      {
+          ans+="1";
+      }
+
+      while(zeros--)
+      {
+         ans+="0"; 
+      }
+      ans+="1";
+      return ans;
+    }
+};
