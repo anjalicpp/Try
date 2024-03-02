7cf9c340bdcf8ed774b81177f76216b40c9698b9
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-03-02 23:03:52 +0530

    Update 2Mar24.cpp

--- Code_2024/March2024/2Mar24.cpp
+++ Code_2024/March2024/2Mar24.cpp
@@ -1,1 +1,7 @@
-
+977. Squares of a Sorted Array
+for(int i=0;i<nums.size();i++)
+        {
+            nums[i]=nums[i]*nums[i];
+        }
+        sort(nums.begin(),nums.end());
+        return nums;
