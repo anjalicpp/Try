b7ab483958eeea17ac1516652663df4255cb1539
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-03-09 22:36:47 +0530

    Update 9Mar24.cpp

--- Code_2024/March2024/9Mar24.cpp
+++ Code_2024/March2024/9Mar24.cpp
@@ -1,1 +1,21 @@
 
+2540. Minimum Common Value
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+       // vector<int>ans;
+        map<int,int>mp;
+        for(int i=0;i<nums1.size();i++)
+        {
+            mp[nums1[i]]++;
+        }
+        for(int j=0;j<nums2.size();j++)
+        {
+            if(mp.find(nums2[j])!=mp.end())
+            {
+                return nums2[j];
+            }
+        }
+      return -1;
+    }
+};
