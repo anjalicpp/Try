78f0229b84822174303211a21b9b173cb6e22097
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-03-10 21:52:18 +0530

    Update 10Mar24.cpp

--- Code_2024/March2024/10Mar24.cpp
+++ Code_2024/March2024/10Mar24.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+       vector<int>ans;
+       unordered_map<int,int>mp;
+       for(auto it:nums1)
+       {
+           mp[it]++;
+       }
+       for(auto it:nums2)
+       {
+           if(mp.find(it)!=mp.end())
+           {
+              ans.push_back(it);
+              mp.erase(it);
+           }
+       }
+       return ans;
+    }
+};
