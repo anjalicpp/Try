e755cb6eb8e4150b2939efd3cc54665597aed9f4
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-03-15 22:22:48 +0530

    Update 15Mar24.cpp

--- Code_2024/March2024/15Mar24.cpp
+++ Code_2024/March2024/15Mar24.cpp
@@ -1,1 +1,27 @@
-
+238. Product of Array Except Self
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+      int n = nums.size();
+         int output[n];
+         vector<int> ans;
+         int product = 1;
+        for(int i=0;i<n;i++) {
+            product*=nums[i];
+            output[i] = product;
+        }
+        product = 1;
+        for(int i=n-1;i>0;i--) {
+            output[i] = output[i-1] * product;
+            product*=nums[i];
+        }
+        output[0] = product;
+       
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back(output[i]);
+        }
+       
+        return ans;   
+    }
+};
