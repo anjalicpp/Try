afda4aea0325ccc9027c16b2daefd91ff079c9c1
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-03-22 20:53:20 +0530

    Update 22Mar24.cpp

--- Code_2024/March2024/22Mar24.cpp
+++ Code_2024/March2024/22Mar24.cpp
@@ -1,1 +1,34 @@
+234. Palindrome Linked List
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* temp=head;
+        vector<int>vec;
+
+        while(temp!=NULL)
+        {
+            vec.push_back(temp->val);
+            temp=temp->next;
+        }
+        
+        int i,j;
+        int n=vec.size();
+        for(i=0,j=n-1;i<=j;i++,j--)
+        {
+            if(vec[i]!=vec[j])return false;
+        }
+        return true;
+        
+    }
+};
