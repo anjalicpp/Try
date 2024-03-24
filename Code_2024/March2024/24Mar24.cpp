dccf67a9fbeb72e32ac1b9cea7df138971383ba0
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-03-24 22:29:14 +0530

    Update 24Mar24.cpp

--- Code_2024/March2024/24Mar24.cpp
+++ Code_2024/March2024/24Mar24.cpp
@@ -1,1 +1,30 @@
+287. Find the Duplicate Number
 
+class Solution {
+public:
+   /* bool check(int index,vector<int>& nums,int num)
+    {
+        for(int i=index;i<nums.size();i++)
+        {
+            if(nums[index]==num)return true;
+        }
+       return false; 
+    }*/
+    //map approach
+    int findDuplicate(vector<int>& nums) {
+       map<int, int> m;
+        int duplicate = 0;
+        for(auto i= 0; i<nums.size(); ++i)
+        ++m[nums[i]];
+        
+        for(auto i : m)
+        {
+            if(i.second > 1)
+            {
+                duplicate = i.first;
+                break;
+            }
+        }
+        return duplicate;
+    }
+};
