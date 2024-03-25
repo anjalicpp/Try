d57ea12fa807ef32f327200f5f7949c6e3c04b5d
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-03-25 22:16:58 +0530

    Update 25Mar24.cpp

--- Code_2024/March2024/25Mar24.cpp
+++ Code_2024/March2024/25Mar24.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+     sort(nums.begin(),nums.end());
+     vector<int>ans;
+    int k=2;
+      int i=0,j=0;
+      while(j<nums.size())
+      {
+          if(j-i+1<k)
+          {
+              j++;
+          }
+          else if(j-i+1==k)
+          {
+              if(nums[i]==nums[j])
+              ans.push_back(nums[i]);
+              i++;j++;
+          }
+      }
+      return ans;  
+    }
+};
