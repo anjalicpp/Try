61054af88eb253593c9665465ba207f0e15ba716
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-03-28 22:38:41 +0530

    Update 28Mar24.cpp

--- Code_2024/March2024/28Mar24.cpp
+++ Code_2024/March2024/28Mar24.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if(nums.size()==1)
+        return nums[0];
+        int pro,maxi=INT_MIN;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+                pro=nums[i]*nums[i+1];
+                maxi=(pro>maxi)?pro:maxi;
+            
+        }
+        return maxi;
+    }
+};
