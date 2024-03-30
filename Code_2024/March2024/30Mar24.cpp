b38494f8910b4aa29987ae272320677306aaf71a
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-03-30 21:26:02 +0530

    Update 30Mar24.cpp

--- Code_2024/March2024/30Mar24.cpp
+++ Code_2024/March2024/30Mar24.cpp
@@ -1,1 +1,23 @@
+2958. Length of Longest Subarray With at Most K Frequency
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+      int ans=0;
+        unordered_map<int,int>mp;
+        int n=size(nums);
 
+        for(int l=0,r=0;r<n;r++){
+            mp[nums[r]]++;
+            if(mp[nums[r]]>k){
+                while(nums[l]!=nums[r]){
+                    mp[nums[l]]--;
+                    l++;
+                }
+                mp[nums[l]]--;
+                l++;
+            }
+            ans=max(ans,r-l+1);
+        }
+        return ans;  
+    }
+};
