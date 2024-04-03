8ec83c21f766776429b636826f273705085e8615
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-04-03 22:07:04 +0530

    Update 3Apr24.cpp

--- Code_2024/April2024/3Apr24.cpp
+++ Code_2024/April2024/3Apr24.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char,char>mp;
+        set<char>st;
+       int n = s.size();
+        for( int i =0;i<n;i++)
+        { 
+            if( mp.find(s[i]) !=mp.end() )
+            {
 
+                if(mp[s[i]]!=t[i])
+                {
+                   return false;
+                }
+                
+            }
+            else{
+               
+                if(st.find(t[i])!=st.end())
+                return false;
+                
+                mp[s[i]]=t[i];
+                st.insert(t[i]);
+            }
+
+        }
+        return true ;
+    }
+};
