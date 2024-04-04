11aeac5c0e3d285f2deb5e7b714276016553b113
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-04-04 23:12:46 +0530

    Update 4April24.cpp

--- Code_2024/April2024/4April24.cpp
+++ Code_2024/April2024/4April24.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+      int ans=0,i=0;
+      int n=s.size();
+      int left=0,right=0;
 
+      while(i<n)
+      {
+        if(s[i]=='(')left++;
+        else if(s[i]==')')right++;
+        ans=max(ans,left-right);
+        i++;
+      }  
+      return ans;
+    }
+};
