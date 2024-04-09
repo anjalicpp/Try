2b3c40e45a49301c4afecbbea1ccc580bba19bf1
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-04-09 20:27:52 +0530

    Update 9April24.cpp

--- Code_2024/April2024/9April24.cpp
+++ Code_2024/April2024/9April24.cpp
@@ -1,1 +1,21 @@
 
+2073. Time Needed to Buy Ticketsclass Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int ans=0;
+        int comp=tickets[k];
+       for(int i=0;i<tickets.size();i++)
+       {
+        if(tickets[i]>comp&&i<=k)
+        {
+            ans+=comp;
+        }
+        else if(tickets[i]>=comp&&i>k)
+        {
+            ans+=comp-1;
+        }
+        else ans+=tickets[i];
+       }
+        return ans;
+    }
+};
