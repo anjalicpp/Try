2b24d844053327186b67c3ee7d2794f2f431a611
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-04-11 15:59:42 +0530

    Update 11April24.cpp

--- Code_2024/April2024/11April24.cpp
+++ Code_2024/April2024/11April24.cpp
@@ -1,3 +1,52 @@
+
+402. Remove K Digits
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+     int n=num.size();
+
+     if(n==k)return "0";
+
+     string ans;
+     vector<int>arr;
+
+     for(int i=0;i<n;i++)
+     {
+        while(k>0&&!arr.empty()&&arr.back()>num[i])
+        {
+            arr.pop_back();//if num in arr are larger than current number pop them out
+            k--;
+        }
+        arr.push_back(num[i]);
+     }
+
+     //if k>0 still then pop more elements
+     while(k>0)
+     {
+        arr.pop_back();
+        k--;
+     }
+
+     //for leading zero case-->0200=200
+
+     for(auto it:arr)
+     {
+        if(it=='0'&&ans.empty())//leading zero case
+        continue;
+
+        ans+=it;
+     }
+     
+     if(ans.size()==0) 
+     {
+        ans.push_back('0');
+     }
+    return ans;
+    }
+};
+
+
+
 2259. Remove Digit From Number to Maximize Result
 
 class Solution {
