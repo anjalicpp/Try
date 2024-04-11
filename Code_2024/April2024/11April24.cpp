9dd90630975502cc609d6ede795e553e5ecbf202
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-04-11 11:42:12 +0530

    Create 11April24.cpp

---
+++ Code_2024/April2024/11April24.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDigit(string number, char digit) {
+       int n=number.length();
+
+       int last_Occur=-1;
+
+       for(int i=0;i<n-1;i++)
+       {
+        if(number[i]==digit)
+        {
+            if(number[i]<number[i+1])
+            {
+                number.erase(i,1);
+                return number;
+            }
+            last_Occur=i;
+        }
+       }
+
+       if(number[n-1]==digit)
+       {
+        last_Occur=n-1;
+       }
+
+       number.erase(last_Occur,1);
+       return number;
+    }
+};
