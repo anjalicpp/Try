2fbd02aea045c450c21f5c3507dcbbc819e1e25b
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-04-13 21:34:11 +0530

    Update 13April24.cpp

--- Code_2024/April2024/13April24.cpp
+++ Code_2024/April2024/13April24.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+ 
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+      vector<int>ans(n,-1);
+      map<int,int>mp;
+      stack<int>st;
 
+      for(auto it:nums2)
+      {
+        while(!st.empty()&&st.top()<it)
+        {
+            mp[st.top()]=it;
+            st.pop();
+        }
+        st.push(it);
+      }
+      for(int i=0;i<n;i++)
+      {
+        if(mp[nums1[i]])
+        {
+            ans[i]=mp[nums1[i]];
+        }
+      }
+      return ans;
+    }
+};
