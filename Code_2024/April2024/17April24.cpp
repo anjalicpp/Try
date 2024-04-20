ad61f05e921575298da3d2f63828dd40bda49b78
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-04-20 23:48:00 +0530

    Update 17April24.cpp

--- Code_2024/April2024/17April24.cpp
+++ Code_2024/April2024/17April24.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0;
+        stack<int>st;
+       for(int i=0;i<s.length();i++)
+       {
+           if(s[i]==' ')
+           {
+               st.push(count);
+               count=0;
+           }
+          else count++;
+       }
+        st.push(count);
+       while(1)
+       {
+           if(st.top()!=0)return st.top();
+        
+        else st.pop();
+       }
+       return st.top();
+      
+    }
+};
