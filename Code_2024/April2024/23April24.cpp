6f4d61e8466127d81ceb0d7a8f5e0627a7466384
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-04-23 23:17:31 +0530

    Update 23April24.cpp

--- Code_2024/April2024/23April24.cpp
+++ Code_2024/April2024/23April24.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
 
+        if(numRows==1||numRows>=s.length())
+        {
+            return s;
+        }
+      int direction=1;
+      int index=0;
+      vector<vector<char>> grid(numRows);
+
+      for(char c:s)
+      {
+        grid[index].push_back(c);
+        if(index==0){
+            direction=1;//from up to down case
+        }
+        else if(index==numRows-1)
+        {
+            direction=-1;//down to up case
+        }
+        index+=direction;
+      }
+
+      string ans;
+      for(auto it:grid)
+      {
+        for(auto c:it)
+        {
+            ans+=c;
+        }
+      }
+      return ans;
+
+    }
+};
