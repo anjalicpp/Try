c88f136de06dc532cb1685269d20b7827fcc66f7
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-05-09 16:58:08 +0530

    Update 9May24.cpp

--- Code_2024/May2024/9May24.cpp
+++ Code_2024/May2024/9May24.cpp
@@ -1,1 +1,25 @@
+3075. Maximize Happiness of Selected Children
 
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+       sort(happiness.begin(),happiness.end());
+       int n=happiness.size();
+       long long ans=0;
+       int del=0;
+       while(k>0)
+       {
+        if(happiness[n-1]-del<0)
+            ans+=0;
+        else
+         {   
+        ans+=happiness[n-1]-del;
+       
+        del++;
+         }
+        k--;
+        n--;
+       }
+    return ans;
+    }
+};
