183b364644179d55303aeef48ca35dda9e144c41
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-05-16 17:04:27 +0530

    Update 16May24.cpp

--- Code_2024/May2024/16May24.cpp
+++ Code_2024/May2024/16May24.cpp
@@ -1,1 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
 
+    bool fun(TreeNode* root)
+    {
+        if(root->val==0||root->val==1)return root->val ;
+        else if(root->val==2)
+        {
+            return fun(root->left)||fun(root->right);
+        }
+        else if(root->val==3)
+        {
+            return fun(root->left)&&fun(root->right);
+        }
+
+    return false;
+    }
+    bool evaluateTree(TreeNode* root) {
+        return fun(root);
+
+    }
+};
