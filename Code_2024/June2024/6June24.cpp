7af924ba3997bc8a1a39795865478a6096dbc3e4
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-06-06 23:53:59 +0530

    Update 6June24.cpp

--- Code_2024/June2024/6June24.cpp
+++ Code_2024/June2024/6June24.cpp
@@ -1,1 +1,38 @@
+846. Hand of Straights
 
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if(hand.size()%groupSize)return false;
+
+        sort(hand.begin(),hand.end());
+
+        map<int,int>mp;
+
+        for(auto it:hand)
+        {
+            mp[it]++;
+        }
+
+        int n=hand.size();
+
+        while(n--)
+        {
+            for(auto it:mp)
+            {
+                int a=it.first;
+
+                for(int i=a;i<a+groupSize;i++)
+                {
+                    if(mp[i]==0)return false;
+                    else mp[i]--;
+
+                    if(mp[i]==0) 
+                    mp.erase(i);
+                }
+                break;
+            }
+        }
+        return true;
+    }
+};
