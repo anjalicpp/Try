8919382c08fdfd992c89b9070f0a4ee9098ff635
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-06-16 23:43:06 +0530

    Update 15June24.cpp

--- Code_2024/June2024/15June24.cpp
+++ Code_2024/June2024/15June24.cpp
@@ -1,1 +1,44 @@
+29. Divide Two Integers
 
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(dividend==divisor)
+            return 1;
+
+        bool sign=true;
+
+        if(dividend>=0 && divisor<0)  sign=false;
+        if(dividend<=0 && divisor>=0)  sign=false;
+
+        long numo=abs(dividend);
+        long deno=abs(divisor);
+        divisor=abs(divisor);
+
+        long quot=0;
+
+        while(numo>=deno)
+        {
+            int count=0;
+
+            while(numo>=(deno<<(count+1)))
+            {
+                count+=1;
+            }
+            quot+=1<<count;
+            numo=numo-(deno<<count);
+        }
+
+        if(quot==(1<<31)&&sign==true)
+        {
+            return INT_MAX;
+        }
+        if(quot==(1<<31)&&sign==false)
+        {
+            return INT_MIN;
+        }
+        return sign?quot:-quot;
+
+
+    }
+};
