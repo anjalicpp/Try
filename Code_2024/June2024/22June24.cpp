7db1de3d63e651d3ac531de91fb823dab09ff253
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-06-22 23:32:44 +0530

    Update 22June24.cpp

--- Code_2024/June2024/22June24.cpp
+++ Code_2024/June2024/22June24.cpp
@@ -1,1 +1,39 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n=nums.size();
+        int i=0,j=0;
+        int odd=0,count=0;
 
+
+        while(j<n)
+        {
+            if(nums[j]%2!=0)
+            {
+                odd++;
+            }
+            if(odd==k)count++;
+            else if(odd>=k)
+            {
+                
+                    while(odd>=k)
+                    {
+                        if(nums[i]%2!=0)
+                        {
+                            i++;
+                            odd--;
+                            if(odd==k)count++;
+                        }
+                        else{
+                            i++;
+                        if(odd==k)count++;
+                        }
+                    }
+                
+
+            }
+            j++;
+        }
+        return count;
+    }
+};
