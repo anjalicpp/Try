b943f8787fd7572af677ff982a04a3c4bfe7a348
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-07-11 23:48:35 +0530

    Create 11July24.cpp

---
+++ Code_2024/July2024/11July24.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newIntervals) {
+        
+        vector<vector<int>>ans;
+        vector<int>mergeIntervals{2,0};
+        int n=intervals.size();
+        int i=0;
+
+        
+        
+            while(i<n&&intervals[i][1]<newIntervals[0])
+            {//function to find left portion
+                ans.push_back(intervals[i]);
+                i++;//
+            }
+            while(i<n&&intervals[i][0]<=newIntervals[1])
+            {//merging middle portion
+                newIntervals[0]=min(newIntervals[0],intervals[i][0]);
+                newIntervals[1]=max(newIntervals[1],intervals[i][1]);
+                i++;
+            }
+            ans.push_back(newIntervals);
+
+            while(i<n)
+            {//pushing right portion as it is
+                ans.push_back(intervals[i]);
+                i++;
+
+            }
+        
+        return ans;
+    }
+};
