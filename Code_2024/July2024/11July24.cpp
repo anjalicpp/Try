86a1c3945fb6d9200febf10b634c570a75359e3c
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-07-13 23:56:16 +0530

    Update 11July24.cpp

--- Code_2024/July2024/11July24.cpp
+++ Code_2024/July2024/11July24.cpp
@@ -34,3 +34,45 @@ public:
         return ans;
     }
 };
+-----------------------
+135. Candy
+
+    class Solution {
+public:
+    int candy(vector<int>& ratings) {
+
+        int n=ratings.size();
+
+        vector<int>left(n,1);
+        vector<int>right(n,1);
+        
+        int sum=0;
+
+        for(int i=1;i<n;i++)//left array filling
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                left[i]=left[i-1]+1;
+            }
+            else{
+                left[i]=1;
+            }
+        }
+
+        for(int j=n-2;j>=0;j--)//right array filling
+        {
+            if(ratings[j]>ratings[j+1])
+            {
+                right[j]=right[j+1]+1;
+            }
+            else{
+                right[j]=1;
+            }
+        }
+        for(int i=0;i<n;i++)//taking max of right and left and adding to final sum
+        {
+            sum+=max(right[i],left[i]);
+        }
+        return sum;
+    }
+};
